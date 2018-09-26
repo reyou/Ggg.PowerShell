ca3260c2298d44b716609817f7cc003d8f996125
reyou <aytekoz@yahoo.com>
2018-09-26 12:33:58 -0400

    Update Ggg.File-Watcher.ps1

--- apps/app-docs/Ggg.File-Watcher.ps1
+++ apps/app-docs/Ggg.File-Watcher.ps1
@@ -1,1 +1,3 @@
-Get-Content -Path "C:\temp\app.log" -Wait
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-content?view=powershell-6
+Get-Content -Path "C:\temp\app.log" -Wait -Tail 5
+
